a53ef0bbc167df27ed8042446da64f92feb995e2
MayceAlazzawi <maynab4@gmail.com>
2022-04-19 19:10:34 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Write your solution in this file!
+// // Write your solution in this file!
+ var customerName = 'bob';
+console.log(customerName);
+
+function upperCaseCustomerName(str) {
+  str.toUpperCase();
+}
+upperCaseCustomerName()
+ 
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+  return bestCustomer ;
+}
+setBestCustomer();
+
+function overwriteBestCustomer() {
+  return bestCustomer = 'maybe bob';
+}
+overwriteBestCustomer();
+
+const leastFavoriteCustomer = 'mayce';
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'hi';
+}
+changeLeastFavoriteCustomer();
